21e1118b60d3e0af83029e6b2007dd4cb3053ed9
M-Ahmad-Sajid <u2024338@giki.edu.pk>
2024-12-20 00:00:56 +0500

    Update main.cpp
    function for name and choice

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
-...
+void playGame() {       // Function of the main game logic and scoreboard
+    string name;
+    int age;
+    
+    cout<<"Hello"<<endl;
+
+    // Get user name and age
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    
+    char choice;// Asking user if he/she wants to play
+    cout << name << ", do you want to play the quiz game? (y/n): ";
+    cin >> choice;
+
+
+    if (choice == 'y' || choice == 'Y') { //checking options
+        int score = askQuestions();  // to get the score from the quiz
+
