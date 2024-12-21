7547c165895d4276a68be3597bc199ab9b650a90
M-Ahmad-Sajid <u2024338@giki.edu.pk>
2024-12-21 17:51:30 +0500

    Update main.cpp
    score and scoreboard

--- main.cpp
+++ main.cpp
@@ -18,4 +18,36 @@ void playGame() {       // Function of the main game logic and scoreboard
 
     if (choice == 'y' || choice == 'Y') { //checking options
         int score = askQuestions();  // to get the score from the quiz
+     // displaying score
+             cout << "\n" << name << " | you scored : " << score;
+
+
+           // saving the name and score in the file
+        ofstream scoreFile;
+          scoreFile.open("scoreboard.txt", ios::app);  // Open in append mode
+        if (scoreFile.is_open()) {
+            scoreFile << name << " " << score<< " out of 20"<< endl;
+            scoreFile.close();  // Close the file after writing
+        } 
+		else {
+            cout << "Error opening the scoreboard file.\n";
+        }
+        
+        
+        cout<<"\nGood Bye";
+        
+
+        //to display the scoreboard
+          cout << "\nScoreboard:\n";
+         ifstream scoreFileRead("scoreboard.txt");
+        string line;
+        while (getline(scoreFileRead, line)) {
+            cout << line << endl;
+        }
+    } 
+	else {
+        cout << "Maybe next time, " << name << "!\n";
+    }
+}
+
 
